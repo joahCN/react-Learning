cd9342ac86dbd68b78c7805be95d6f884ed28183
joah.zhang <448363903@qq.com>
2015-05-21 17:52:02 +0800

    auto reload

--- js/main.jsx
+++ js/main.jsx
@@ -1,11 +1,52 @@
 import React from "react";
+require("../css/main.less");
 
 class CommentBox extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			imags: [
+				{url: "http://placehold.it/150x150", desc: "hello world"},
+				{url: "http://placehold.it/150x150", desc: "It's rainy"},
+				{url: "http://placehold.it/150x150", desc: "weekend is comming"},
+				{url: "http://placehold.it/150x150", desc: "hot deployment"}
+			]
+		};
+		this.handleLoadMore = () =>  {
+			var additionals = [
+				{url: "http://placehold.it/150x150", desc: "additional"},
+				{url: "http://placehold.it/150x150", desc: "additional"}
+			];
+			this.setState({imags: this.state.imags.concat(additionals)});
+		}
+	}
+
+	componentDidMount() {
+		console.log("component mount");
+	}
+	
 	render() {
-		return <div>
-			hello , react
-		</div>;
+		var items = this.state.imags.map(function(img) {
+			return <ImageItem {...img} /> 
+		});
+		return (<div>
+			{items}
+			<button onClick={this.handleLoadMore}>show more</button>
+		</div>);
 	}
 }
+
+class ImageItem extends React.Component {
+	getInitialState() {
+		return {};
+	}
+	render() {
+		return (<div className="imageItem">
+			<img src={this.props.url}/>
+			<p style={{textAlign: "center", color: "red", fontSize: "15px"}}>{this.props.desc}</p>
+		</div>);
+	}
+}
+
 
 React.render(<CommentBox />, document.getElementById("content"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
 var Path = require("path");
+var ExtractTextPlugin  = require("extract-text-webpack-plugin");
 
 module.exports = {
     context: Path.join(__dirname, "js"),
-    entry: "./main.jsx",
+    entry: ['webpack/hot/dev-server', "./main.jsx"],
     output: {
-        path: Path.join(__dirname, "js"),
-        filename: "bundle.js"
+        path: Path.join(__dirname, "build"),
+        filename: "bundle.js",
+        publicPath: "/build/"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+            { test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader") },
 			{ test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"}
         ]
     },
+    plugins: [
+        new ExtractTextPlugin("[name].css")
+    ],
+    resolve: {
+        root: __dirname,
+        extensions: ["", ".webpack.js", ".web.js", ".js", ".less"]
+    },
+    devServer: {
+        contentBase: __dirname,
+        noInfo: true, //  --no-info option
+        hot: true
+        //,inline: true
+    }
 	
 };
