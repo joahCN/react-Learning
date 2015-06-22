7ec5a89d0b934ec750e5e8fc0494adff30c57aef
joahCN <448363903@qq.com>
2015-06-22 23:02:40 +0800

    add server file

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 8888 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
